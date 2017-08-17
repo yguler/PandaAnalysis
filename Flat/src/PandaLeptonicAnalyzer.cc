43f48da7657e86cd3e7bceffa810bfb61bbf5d9d
Sid Narayanan <sidn@mit.edu>
2017-08-17 17:21:34 -0400

    migrating function definitions out of headers

--- Flat/src/PandaLeptonicAnalyzer.cc
+++ Flat/src/PandaLeptonicAnalyzer.cc
@@ -7,10 +7,37 @@
 #include "TSystem.h"
 #include "TRandom2.h"
 
-#include "PandaAnalysis/Flat/interface/LeptonicFactors.h"
-
 using namespace panda;
 using namespace std;
+
+
+
+double weightEWKCorr(float pt, int type){
+  double parWZ08[2] = { 2.85714,-0.05714};
+  double parZZ08[2] = {-4.57143,-0.06857};
+  double parWZ14[3] = {3.69800,-0.0726117,0.0000318044};
+  double parZZ14[3] = {-0.586985000,-0.099845900,0.0000445083};
+  double corrA = 0.0;
+  double corrB = 0.0;
+  if     (type == 0){ // WZ13
+    corrA = (parWZ08[0]+parWZ08[1]*pt)/100.;
+    corrB = (parWZ14[0]+parWZ14[1]*pt+parWZ14[2]*pt*pt)/100.;
+  }
+  else if(type == 1){ // ZZ13
+    corrA = (parZZ08[0]+parZZ08[1]*pt)/100.;
+    corrB = (parZZ14[0]+parZZ14[1]*pt+parZZ14[2]*pt*pt)/100.;
+  }
+  double corr = corrB - (corrB-corrA)/6.;
+
+  if(corr >= 0.0) return 1.0;
+  return (1.0+corr);
+}
+
+
+double weightZHEWKCorr(float baseCorr){
+  return (baseCorr+0.31+0.11)/((1-0.053)+0.31+0.11);
+}
+
 
 PandaLeptonicAnalyzer::PandaLeptonicAnalyzer(int debug_/*=0*/) {
   DEBUG = debug_;

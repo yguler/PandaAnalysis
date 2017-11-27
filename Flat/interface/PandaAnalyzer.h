3acf315545f881e2419164f837df7817c0518d4c
Benedikt Maier <benedikt.maier@cern.ch>
2017-11-27 17:00:15 -0500

    Removing TFactory

--- Flat/interface/PandaAnalyzer.h
+++ Flat/interface/PandaAnalyzer.h
@@ -33,7 +33,7 @@
 #include "PandaAnalysis/Utilities/interface/CSVHelper.h"
 
 // TMVA
-#include "TMVA/Factory.h"
+//#include "TMVA/Factory.h"
 #include "TMVA/Tools.h"
 #include "TMVA/Reader.h"
 #include "TMVA/MethodCuts.h"

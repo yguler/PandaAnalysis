34c91605c3136ac6c8c386e5f4bf2df97d6766d7
Sid Narayanan <sidn@mit.edu>
2017-11-29 14:30:45 -0500

    restoring multiple evt slices

--- Deep/scripts/extract_evt.py
+++ Deep/scripts/extract_evt.py
@@ -8,20 +8,26 @@ from PandaCore.Tools.Misc import PInfo, PWarning
 flist = glob(argv[2])
 target_evt = np.int64(argv[1])
 
+lumis = []
+pts = []
+msds = []
+pfs = []
+
 for f in flist:
     arr = np.load(f)
     evt = arr['eventNumber']
     mask = (evt == target_evt)
     if np.sum(mask):
         idx = np.argmax(mask)
-        pf = arr['pf'][idx]
-        msd = arr['msd'][idx]
-        pt = arr['pt'][idx]
-        found = evt[idx]
-        np.savez('sliced.npz', pf=pf, msd=msd, pt=pt, evt=found)
+        pfs.append(arr['pf'][idx])
+        pts.append(arr['pt'][idx])
+        msds.append(arr['msd'][idx])
+        lumis.append(arr['lumi'])
         PInfo(argv[0], 'Found %i in %s'%(target_evt, f))
-        exit(0)
 
 
-PWarning(argv[0], 'Could not find %i in %s'%(target_evt, argv[2]))
-exit(1)
+if lumis:
+    np.savez('sliced.npz', pf=np.array(pfs), msd=np.array(msds), pt=np.array(pts), lumi=np.array(lumis))
+else:
+    PError(argv[0], 'Could not find %i in %s'%(target_evt, argv[2]))
+    exit(1)

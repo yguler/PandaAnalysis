403525b9ced3c2f98ab7c610fc9ede4c19e68547
Sid Narayanan <sidn@mit.edu>
2017-11-30 16:06:18 -0500

    moving to lfs

---
+++ data/vbf16/kqcd/mjj/merge.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:c9d0d6853b3ebac27e7b913c5db88336d73df945e7cad018d782755e5ab21da0
+size 1117
---
+++ data/vbf16/trig/merge_effs.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1a7258e33136e43d4f1d0e4bfc1020c43f717742fdd8e2f270d9fa2c7a3df63b
+size 1457

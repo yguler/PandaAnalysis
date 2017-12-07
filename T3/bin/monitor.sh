13ed99a5eb09a35b771851705a5f4980ba86f3b3
Sid Narayanan <sidn@mit.edu>
2017-12-07 11:19:45 -0500

    refresh every 60s even if there is no update

--- T3/bin/monitor.sh
+++ T3/bin/monitor.sh
@@ -5,11 +5,12 @@ LAST=1
 while true
 do
     RECENT=$(stat -c%Z $(ls -ltr $SUBMIT_LOCKDIR/* | tail -n1 | awk '{ print $9; }'))
-    if [[ $RECENT > $LAST ]]; 
+    if [[ $RECENT > $LAST || $(expr $(date +%s) - $LAST) > 60 ]]; 
     then
         clear
         ${CMSSW_BASE}/src/PandaAnalysis/T3/bin/checkMissingFiles.py
-        LAST=$RECENT
+        LAST=$(date +%s) # this takes care of the 60s refresh
+        date
     fi
     sleep 5
 done

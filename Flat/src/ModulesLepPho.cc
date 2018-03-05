f16ebe90f3f4a8016985b6d142503ef7905901b3
Dylan George Hsu <dghsu@mit.edu>
2018-03-05 15:14:09 -0500

    Add pithy disclaimer

--- Flat/src/ModulesLepPho.cc
+++ Flat/src/ModulesLepPho.cc
@@ -189,9 +189,10 @@ void PandaAnalyzer::ComplicatedLeptons() {
     looseLeps.push_back(&ele);
     matchLeps.push_back(&ele);
     matchEles.push_back(&ele);
-    gt->nLooseElectron++;
+    // WARNING: The definition of "loose" here may not match your analysis definition of a loose electron for lepton multiplicity or jet cleaning considerations.
+    // It is the user's responsibility to make sure he is cutting on the correct multiplicity. Enough information is provided to do this downstream.
+    gt->nLooseElectron++; 
     if (gt->nLooseElectron>=NLEP) break;
-
   }
 
   // muons
@@ -267,6 +268,8 @@ void PandaAnalyzer::ComplicatedLeptons() {
     matchLeps.push_back(&mu);
     TVector2 vMu; vMu.SetMagPhi(pt,mu.phi());
     vMETNoMu += vMu;
+    // WARNING: The definition of "loose" here may not match your analysis definition of a loose electron for lepton multiplicity or jet cleaning considerations.
+    // It is the user's responsibility to make sure he is cutting on the correct multiplicity. Enough information is provided to do this downstream.
     gt->nLooseMuon++;
     if (gt->nLooseMuon>=NLEP) break;
   }
